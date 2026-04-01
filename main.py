892864532373da17000e6bae184f0a4b6786e14e
mrodenas <marodenas@asbarcelona.com>
2026-03-31 19:54:18 -0700

    modified data to automaticallly be sent and displayed in html

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import json
-
-import json
+from jinja2 import Environment, FileSystemLoader
 
 with open('datasetp1.json', 'r') as file:
-    jsondata = json.load(file)
+    data = json.load(file)
 
-    for item in jsondata:
-        print(f"Product: {item.get('productName')}")
-        print(f"Color: {', '.join(item.get('colors', []))}")
-        print(f"Price: {item.get('price')} {item.get('priceCurrency')}")
-        print(f"Size: {item.get('sizeLabel')}")
-        print(f"Country: {item.get('country')}")
-        print(f"Image: {item.get('imageUrl')}")
-        print(f"URL: {item.get('productUrl')}")
-        print(f"Description: {item.get('description').strip()}")
-        print("-" * 30)
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template('home.html')
+html_output = template.render(products=data)
+
+with open('home.html', 'w', encoding='utf-8') as f:
+    f.write(html_output)
